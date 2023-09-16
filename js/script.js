ff172fccf50f422cfb03dfd56c5c3c730ee4588c
Leggi <leandrosalmeida@gmail.com>
2023-09-16 12:55:06 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function gerarSenha() {
+  var password = "";
+  var str =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy" + "0123456789" +"|`!+_-`@#$/%*<>{}[]^~";
+
+  for (i = 1; i <= 10; i++) {
+    var char = Math.floor(Math.random() * str.length );
+
+    password += str.charAt(char);
+  }
+  return password;
+  
+}
+
+
+function adcionarSenha(){
+  document.getElementById('nova-senha').innerHTML = gerarSenha();
+  toggle();
+}
+
+function toggle() {
+  var senhaGerada = document.getElementById('senha-gerada');
+  senhaGerada.style.display = senhaGerada.style.display === 'none' ? 'flex' : 'none';
+}
+
